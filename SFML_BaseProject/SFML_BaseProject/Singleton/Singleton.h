cd83176eb80672534858528fe03f94f18cddfabc
Domergue31 <114091389+Domergue31@users.noreply.github.com>
2023-10-10 10:03:13 +0200

    add Object et Singleton

--- SFML_BaseProject/SFML_BaseProject/Singleton/Singleton.h
+++ SFML_BaseProject/SFML_BaseProject/Singleton/Singleton.h
@@ -1,1 +1,25 @@
 #pragma once
+#include "../Object.h"
+
+template<typename T>
+class Singleton : public Object
+{
+	static inline T* instance = nullptr;
+public:
+	Singleton() = default;
+	virtual ~Singleton() override
+	{
+		OnDestroy();
+	}
+public:
+	static T* Instance()
+	{
+		if (!instance)
+			instance = new T();
+		return instance;
+	}
+	void OnDestory()
+	{
+		delete instance;
+	}
+};
